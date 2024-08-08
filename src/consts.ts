a584281d374c8607460aee6a171f9abab2ef66f6
JuanHeis <juaany98@gmail.com>
2024-08-08 20:26:06 -0300

    base

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Recetas";
+export const SITE_DESCRIPTION = "Recetas de cocina con Libertad";
